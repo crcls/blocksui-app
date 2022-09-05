ae81c8abff4d4cf941b7c763a218e158f2d8c826
Karl Pawlowicz <karlpawlowicz@gmail.com>
2022-09-05 12:57:39 -0400

    feat: add scaffolding

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    };
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
