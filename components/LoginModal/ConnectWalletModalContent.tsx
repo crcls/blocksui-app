51dd6c4828604583401e139e0853bddc34f36024
Karl Pawlowicz <karlpawlowicz@gmail.com>
2022-09-21 17:55:42 -0400

    feat: capitalize heading

--- components/LoginModal/ConnectWalletModalContent.tsx
+++ components/LoginModal/ConnectWalletModalContent.tsx
@@ -4,7 +4,7 @@ const ConnectWalletModalContent = () => {
   return (
     <>
       <h2 className="text-xl font-medium text-neutral-900 sm:pr-12">
-        Connect wallet
+        Connect Wallet
       </h2>
       <p>Connect with one of our available wallet providers.</p>
       <ul role="list" className="mt-4 divide-y divide-neutral-200">
