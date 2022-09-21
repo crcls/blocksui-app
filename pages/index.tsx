46d00cbf58c86d8485515b42c8ca4bad47d35090
Karl Pawlowicz <karlpawlowicz@gmail.com>
2022-09-21 13:14:09 -0400

    feat: update sdk

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { BlockContainer } from '@crcls/blocksui-sdk'
 
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <BlockContainer />
       <Header />
       <Hero />
       <Footer />
