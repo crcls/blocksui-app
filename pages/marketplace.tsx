3282f8629ec7147899a88c494007f74f59dcb0b2
Matt Grannary <me@mattgrannary.com>
2022-09-22 03:04:03 -0400

    feat: pull listings from the BUIMarketplace contract

--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react'
+import { FC, Fragment, useCallback, useState, useEffect } from 'react'
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import {
@@ -11,10 +11,16 @@ import {
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import clsx from 'clsx'
+import { useMoralis, useChain, useWeb3Contract } from 'react-moralis'
+import { BigNumber } from 'ethers'
 
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import Logo from '@/components/Logo'
+
+import useContracts from '../hooks/use-contracts'
+import useIPFS from '../hooks/use-ipfs'
+import { resolver } from '../utils/async'
+import { uint8ToStr, readFile } from '../utils/bytes'
 
 const sortOptions = [
   { name: 'Most Popular', href: '#', current: true },
@@ -41,33 +47,170 @@ const filters = [
     ],
   },
 ]
-const products = [
-  {
-    id: 1,
-    name: 'MoonMail Contact Form',
-    href: '/block/1',
-    price: '0.33 ETH',
-    description:
-      'A simple MoonMail contact form block with some validation that you can embed in your website',
-    imageSrc: '',
-    imageAlt:
-      'MoonMail Contact Form – A simple MoonMail contact form block with some validation that you can embed in your website',
-  },
-  {
-    id: 2,
-    name: 'MoonMail Contact Form',
-    href: '/block/2',
-    price: '0.33 ETH',
-    description:
-      'A simple MoonMail contact form block with some validation that you can embed in your website',
-    imageSrc: '',
-    imageAlt:
-      'MoonMail Contact Form – A simple MoonMail contact form block with some validation that you can embed in your website',
-  },
-]
+
+function ipfsToHttp(ipfs?: string): string {
+  if (!ipfs) return ''
+  return `https://ipfs.io/ipfs/${ipfs.split('//')[1]}`
+}
+
+interface ListingBase {
+  licensable: boolean
+  metaDataURI: string
+  owner: string
+  pricePerDay: BigNumber
+  price?: BigNumber
+}
+
+interface Listing extends ListingBase {
+  description?: string
+  href?: string
+  image?: string
+  name?: string
+}
+
+const ListingItems: FC<{ address: string; abi: any }> = ({ address, abi }) => {
+  const [listings, setListings] = useState<Listing[]>([])
+  const [error, setError] = useState<Error | null>(null)
+  const { getWeb3Storage } = useIPFS()
+  const { data, runContractFunction, isLoading } = useWeb3Contract({
+    abi,
+    contractAddress: address,
+    functionName: 'getListings',
+    params: {
+      amount: 25,
+      page: 0, // TODO: add pagination
+    },
+  })
+
+  const resolveMetadata = useCallback(
+    async (items: ListingBase[]): Promise<Listing[]> => {
+      const result = items.reduce<Promise<any[]>>(async (memo, listing) => {
+        if (listing.owner === '0x0000000000000000000000000000000000000000')
+          return await Promise.resolve(memo)
+
+        const cid = listing.metaDataURI.split('//')[1].split('/')[0]
+        const [err, files] = await resolver(getWeb3Storage(cid))
+
+        if (err !== undefined || files === undefined) {
+          throw new Error('Failed to fetch metadata')
+        }
+
+        const data = new Uint8Array(await readFile(files[0]))
+        const json = uint8ToStr(data, 'utf8')
+        const metadata = JSON.parse(json)
+
+        return await Promise.resolve([
+          ...(await memo),
+          {
+            ...listing,
+            ...metadata,
+          },
+        ])
+      }, Promise.resolve([]))
+
+      return await result
+    },
+    [getWeb3Storage]
+  )
+
+  useEffect(() => {
+    if (data === null && !isLoading) {
+      runContractFunction()
+        .then(async (data) => {
+          if (data) {
+            resolveMetadata(data as ListingBase[])
+              .then(setListings)
+              .catch(setError)
+          }
+        })
+        .catch(setError)
+    }
+  }, [resolveMetadata, data, runContractFunction, isLoading])
+
+  if (error) {
+    return <p>{error.message}</p>
+  }
+
+  return (
+    <>
+      {listings.map((listing, i) => (
+        <a
+          key={`blockListing-${i}`}
+          href={listing.href}
+          className="group text-sm"
+        >
+          <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-neutral-100 group-hover:opacity-75">
+            <img
+              src={ipfsToHttp(listing.image)}
+              alt={listing.name}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+          <h3 className="mt-4 font-medium text-black">{listing.name}</h3>
+          <p className="italic text-neutral-500">{listing.description}</p>
+          <p className="mt-2 font-medium text-black">
+            {listing.licensable
+              ? listing.pricePerDay.toString()
+              : listing.price?.toString()}
+          </p>
+        </a>
+      ))}
+    </>
+  )
+}
 
 const Marketplace: NextPage = () => {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+  const { switchNetwork, chain } = useChain()
+  const [error, setError] = useState<Error | null>(null)
+  const { enableWeb3, isWeb3Enabled } = useMoralis()
+  const [loading, setLoading] = useState(false)
+  const [provider, setProvider] = useState<any | null>(null)
+  const { getContractABI, contractsLoaded } = useContracts()
+  const [config, setConfig] = useState<Record<string, any> | null>(null)
+
+  useEffect(() => {
+    if (!loading) {
+      if (!isWeb3Enabled) {
+        setLoading(true)
+        console.log('Enabling Web3')
+        enableWeb3({
+          chainId: 80001,
+        })
+          .then((resp) => {
+            setProvider(resp)
+            setLoading(false)
+          })
+          .catch((e) => {
+            setError(e)
+            setLoading(false)
+          })
+      } else if (chain?.networkId !== 80001) {
+        setLoading(true)
+        setError(new Error('Switching network to Polygon Mumbai'))
+        switchNetwork('0x13801').catch(console.error)
+        setLoading(false)
+      }
+    }
+  }, [
+    loading,
+    setLoading,
+    provider,
+    switchNetwork,
+    chain,
+    enableWeb3,
+    isWeb3Enabled,
+  ])
+
+  useEffect(() => {
+    if (contractsLoaded && config === null) {
+      try {
+        setConfig(getContractABI('BUIMarketplace'))
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }, [getContractABI, contractsLoaded, config])
 
   return (
     <>
@@ -261,7 +404,7 @@ const Marketplace: NextPage = () => {
         </div>
         <section aria-labelledby="products-heading" className="pt-6 pb-24">
           <h2 id="products-heading" className="sr-only">
-            Products
+            Blocks
           </h2>
           <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
             <form className="hidden lg:block">
@@ -335,29 +478,10 @@ const Marketplace: NextPage = () => {
               ))}
             </form>
             <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:col-span-3 lg:gap-x-8">
-              {products.map((product) => (
-                <a
-                  key={product.id}
-                  href={product.href}
-                  className="group text-sm"
-                >
-                  <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-neutral-100 group-hover:opacity-75">
-                    {/* <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
-                      className="h-full w-full object-cover object-center"
-                    /> */}
-                    <Logo className="h-full w-full object-cover object-center p-24" />
-                  </div>
-                  <h3 className="mt-4 font-medium text-black">
-                    {product.name}
-                  </h3>
-                  <p className="italic text-neutral-500">
-                    {product.description}
-                  </p>
-                  <p className="mt-2 font-medium text-black">{product.price}</p>
-                </a>
-              ))}
+              {error && <p>error.message</p>}
+              {config !== null && (
+                <ListingItems address={config.address} abi={config.abi} />
+              )}
             </div>
           </div>
         </section>
